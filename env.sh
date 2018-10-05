f2bbe9cee0f403588283633d878aadd904f01ff4
Danny Thomson <dthomson25@gmail.com>
2018-10-05 07:45:35 -0700

    generatic kubeflow

---
+++ env.sh
@@ -0,0 +1,7 @@
+PLATFORM=generatic
+KUBEFLOW_REPO=/Users/dthomson/Development/kubeflow/v0.3.0/kubeflow-0.3.0
+KUBEFLOW_VERSION=0.3.0
+KUBEFLOW_KS_DIR=/Users/dthomson/Development/kubeflow/v0.3.0/kubeflow-0.3.0/scripts/myapp/ks_app
+KUBEFLOW_DOCKER_REGISTRY=
+K8S_NAMESPACE=kubeflow
+KUBEFLOW_CLOUD=null
