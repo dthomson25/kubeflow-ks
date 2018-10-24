a5af9adea4d012d88c72022d0f84740817127fe4
Danny Thomson <dthomson25@gmail.com>
2018-10-24 13:19:47 -0700

    Initial Kubeflow ksonnet files

---
+++ env.sh
@@ -0,0 +1,8 @@
+PLATFORM=minikube
+KUBEFLOW_REPO=/Users/dthomson/Development/kubeflow/tmp2
+KUBEFLOW_VERSION=master
+KUBEFLOW_KS_DIR=/Users/dthomson/Development/kubeflow/tmp2/minikube-test/ks_app
+KUBEFLOW_DOCKER_REGISTRY=
+K8S_NAMESPACE=kubeflow
+KUBEFLOW_CLOUD=minikube
+MOUNT_LOCAL=false
